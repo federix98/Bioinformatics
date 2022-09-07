dfbd729bb2c70506b523977caadd049c10f5eb87
federix98 <fededm43@gmail.com>
2022-09-07 08:22:28 +0200

    update pipeline

--- miRNA_Seq/pipeline.sh
+++ miRNA_Seq/pipeline.sh
@@ -34,13 +34,13 @@ conda activate bioinf
 
 for i in 70459 70460 70461 70462 74665 74666 74667 74668 74669 74670 74671 74672 74673 74674 74675 74676 74677 74678 74679 74680 74681 74682 74683 74684
 do
-    # echo "Running FastQC on raw sample $i now"
-    # fastqc -o $INPUTDIR/QCReports $INPUTDIR/$i".fastq.gz" -t 16
-    # mkdir $OUTPUTDIR/$i
-    # echo "Sample $i: Running of UMI extraction with regex on raw reads"
-    # umi_tools extract --stdin=$INPUTDIR/$i".fastq.gz" --log=$OUTPUTDIR/$i/$i"-UMIextraction-fromrawreads.log" --stdout=$OUTPUTDIR/$i/$i"-directUMIextracted.fastq" --extract-method=regex --bc-pattern='.+(?P<discard_1>AACTGTAGGCACCATCAAT){s<=2}(?P<umi_1>.{12})(?P<discard_2>.+)'
-    # echo "Sample $i: Applying a read length filter to remove too short and long reads"
-    # cutadapt --minimum-length=$MINLENGTH --maximum-length=$MAXLENGTH -o $OUTPUTDIR/$i/$i"-directUMIextracted-min"$MINLENGTH"max"$MAXLENGTH"L.fastq" $OUTPUTDIR/$i/$i"-directUMIextracted.fastq" > $OUTPUTDIR/$i/$i"-directUMIextracted-readlengthfilter-cutadapt[min"$MINLENGTH"max"$MAXLENGTH"L].log"
+    echo "Running FastQC on raw sample $i now"
+    fastqc -o $INPUTDIR/QCReports $INPUTDIR/$i".fastq.gz" -t 16
+    mkdir $OUTPUTDIR/$i
+    echo "Sample $i: Running of UMI extraction with regex on raw reads"
+    umi_tools extract --stdin=$INPUTDIR/$i".fastq.gz" --log=$OUTPUTDIR/$i/$i"-UMIextraction-fromrawreads.log" --stdout=$OUTPUTDIR/$i/$i"-directUMIextracted.fastq" --extract-method=regex --bc-pattern='.+(?P<discard_1>AACTGTAGGCACCATCAAT){s<=2}(?P<umi_1>.{12})(?P<discard_2>.+)'
+    echo "Sample $i: Applying a read length filter to remove too short and long reads"
+    cutadapt --minimum-length=$MINLENGTH --maximum-length=$MAXLENGTH -o $OUTPUTDIR/$i/$i"-directUMIextracted-min"$MINLENGTH"max"$MAXLENGTH"L.fastq" $OUTPUTDIR/$i/$i"-directUMIextracted.fastq" > $OUTPUTDIR/$i/$i"-directUMIextracted-readlengthfilter-cutadapt[min"$MINLENGTH"max"$MAXLENGTH"L].log"
     echo "Running of bowtie to mirbase mature seq"
     bowtie -n 0 -l 30 --norc --best --strata -m 1 --threads 16 $HUMANMIRBASEINDEX/mature $OUTPUTDIR/$i/$i"-directUMIextracted-min"$MINLENGTH"max"$MAXLENGTH"L.fastq" --un $OUTPUTDIR/$i/$i"-maturemiRNA-unalignedReads-bowtie1-beststratam1.fastq" -S $OUTPUTDIR/$i/$i"-maturemiRNA-aligned-bowtie1-beststratam1.sam" 2> $OUTPUTDIR/$i/$i"-bowtie-maturemiRNA-beststratam1.log"
     echo "Sorting and indexing the BAM file for counting the mirna occurences"
